b59b051e6f52aaaad637e8ac63f0307e49438be2
nknaleena101 <nknaleena101@gmail.com>
2025-05-08 12:24:14 +0530

    2nd commit

---
+++ src/main/java/warehouse/util/DatabaseUtil.java
@@ -0,0 +1,20 @@
+package warehouse.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/swift_link_wms";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
